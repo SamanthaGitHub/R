7d2b78d7eaa322ac035a453d00d61abf60f67507
Samantha <samanthax0326@gmail.com>
2023-05-10 16:07:57 -0700

    programming basics

---
+++ R-Programming-AtoZ-Udemy/Section1/DataTypes.R
@@ -0,0 +1,57 @@
+
+#integer
+x <- 2L
+typeof(x)
+
+
+#double (decimal)
+y <- 2.5
+typeof(y)
+
+#by default, single variables will be stored as double
+
+x+x
+x+y
+typeof(x+y)
+
+
+#complex
+z <- 3 + 2i
+typeof(z)
+
+
+#character
+a <- "hello world"
+typeof(a)
+
+
+#logical
+q1 <- T
+typeof(q1)
+
+q2 <- FALSE
+typeof(q2)
+
+
+
+
+
+A <- 10
+B <- 5
+c <- A + B
+c
+
+var1 <- 2.5
+var2 <- 4
+
+result <- var1/var2
+result
+
+answer <- sqrt(var2)
+answer
+
+greeting <- "hello"
+name <- "sam"
+
+message <- paste(greeting,name)
+message
