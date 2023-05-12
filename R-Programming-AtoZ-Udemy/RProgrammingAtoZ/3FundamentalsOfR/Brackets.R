8c6fa217f5ff1786a9dc98134e5cdc3eb3cc5859
Samantha <samanthax0326@gmail.com>
2023-05-12 08:13:07 -0700

    brackets

---
+++ R-Programming-AtoZ-Udemy/RProgrammingAtoZ/3FundamentalsOfR/Brackets.R
@@ -0,0 +1,15 @@
+w <- c("a", "b", "c", "d", "e")
+w
+w[1]
+w[5]
+
+w[-1] #all except first
+w[-3] #all except third
+
+w[1:3] #indices 1-3
+
+w[c(1, 3, 5)] #first, third
+w[c(-2, -4)] #same as above
+
+w[-3:-5] #omits 3-5 indexes
+w[1:2] #same as above
