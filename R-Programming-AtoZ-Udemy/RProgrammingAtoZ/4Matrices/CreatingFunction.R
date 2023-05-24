e413cde2e3a65352672246b83da67b323177f07e
Samantha <samanthax0326@gmail.com>
2023-05-24 15:08:20 -0700

    modify function

--- R-Programming-AtoZ-Udemy/RProgrammingAtoZ/4Matrices/CreatingFunction.R
+++ R-Programming-AtoZ-Udemy/RProgrammingAtoZ/4Matrices/CreatingFunction.R
@@ -9,10 +9,14 @@ legend("bottomleft", inset=0.01, legend=Players[1], col=c(1:4, 6), pch=15:18, ho
 #myplot Function with player and stat parameters
 myplot <- function(stat,rows=1:10){ #defaults all players if no rows selected
   Data <- stat[rows,,drop=F] #subsetting
-  
   matplot(t(Data), type="b", pch=15:18, col=c(1:4, 6))
   legend("bottomleft", inset=0.01, legend=Players[rows], col=c(1:4, 6), pch=15:18, horiz=F)
 }
-myplot(Salary)
 
+myplot(Salary)
 myplot(Games)
+
+myplot <- function(z,who=1:10){
+  matplot(t(z[who,,drop=F]), type="b", pch=15:18, col=c(1:4, 6), main="Basketball")
+  legend("bottomleft", inset=0.01, legend=Players[rows], col=c(1:4, 6), pch=15:18, horiz=F)
+}
