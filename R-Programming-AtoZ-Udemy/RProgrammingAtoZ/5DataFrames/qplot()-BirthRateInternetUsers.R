fe7a03ba9d6dc9d91cd30ca2db70725af79f29c1
Samantha <samanthax0326@gmail.com>
2023-06-17 18:19:03 -0700

    adding shape, transparency, and titles to qplots

--- R-Programming-AtoZ-Udemy/RProgrammingAtoZ/5DataFrames/qplot()-BirthRateInternetUsers.R
+++ R-Programming-AtoZ-Udemy/RProgrammingAtoZ/5DataFrames/qplot()-BirthRateInternetUsers.R
@@ -20,3 +20,27 @@ qplot(data=stats2, x=Internet.users, y=Birth.rate,
 
 qplot(data=stats2, x=Internet.users, y=Birth.rate, #colors by the factors in Income.Group
       color= Income.Group, size=I(2))
+
+
+
+
+
+## Visualizing with new Split (after merging in region data)
+qplot(data=merged.stats2.df, x=Internet.users, y=Birth.rate,
+      color=Region, size=I(2)) #just gorgeous
+
+#shapes
+qplot(data=merged.stats2.df, x=Internet.users, y=Birth.rate,
+      color=Region, size=I(2), shape=I(25)) #hollow triangle for shape; shapes range 0:25
+
+#transparency
+qplot(data=merged.stats2.df, x=Internet.users, y=Birth.rate,
+      color=Region, size=I(2), shape=I(19), alpha=I(0.5)) #alpha/transparency 0:1
+
+#adding a title
+qplot(data=merged.stats2.df, x=Internet.users, y=Birth.rate,
+      color=Region, size=I(2), shape=I(19), alpha=I(0.5), main="Birth Rate v Internet Users")
+
+
+
+
